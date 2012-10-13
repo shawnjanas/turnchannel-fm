4829faafaf89a5564db70102486fdde59cdf7557
shawnjanas <janasshawn@gmail.com>
2012-10-12 20:31:29 -0400

    Remove model fetch

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,12 +2,6 @@ class TracksController < ApplicationController
   # GET /tracks
   # GET /tracks.json
   def index
-    @tracks = Track.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @tracks }
-    end
   end
 
   def genre
