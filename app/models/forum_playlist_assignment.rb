741eec6c3ae8b1585b7fee9343a6628a038d169e
shawnjanas <janasshawn@gmail.com>
2012-10-29 12:37:23 -0400

    remove files

--- app/models/forum_playlist_assignment.rb
+++
@@ -1,6 +0,0 @@
-class ForumPlaylistAssignment < ActiveRecord::Base
-  attr_accessible :forum_id, :playlist_id
-
-  belongs_to :forum
-  belongs_to :playlist
-end
--- app/models/track_playlist_assignment.rb
+++
@@ -1,6 +0,0 @@
-class TrackPlaylistAssignment < ActiveRecord::Base
-  attr_accessible :track_id, :playlist_id
-
-  belongs_to :track
-  belongs_to :playlist
-end
