b0954f375ef1d9d6a8f68d7dac6b0db0a3ff2c9c
shawnjanas <janasshawn@gmail.com>
2012-10-30 00:27:03 -0400

    ...

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -12,10 +12,6 @@ unless ENV['RAILS_ENV'] == 'test'
   rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
   rails_env = ENV['RAILS_ENV'] || 'development'
 
-
-  resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-  Resque.redis = resque_config[rails_env]
-
   Dir["#{Rails.root}/lib/jobs/*.rb"].each { |file| require file }
 
   # Load Resque schedule
@@ -29,6 +25,4 @@ unless ENV['RAILS_ENV'] == 'test'
       [username, password] == ["turnchannelfm", "ab20cd20"]
     end
   end
-
-  Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
 end
--- lib/tasks/resque.rake
+++ lib/tasks/resque.rake
@@ -2,5 +2,13 @@ require 'resque/tasks'
 require 'resque_scheduler/tasks'
 
 task "resque:setup" => :environment do
-  Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
+  ENV['QUEUE'] = '*'
+
+  Resque.after_fork do |job|
+    ActiveRecord::Base.establish_connection
+  end
+
 end
+
+desc "Alias for resque:work (To run workers on Heroku)"
+task "jobs:work" => "resque:work"
