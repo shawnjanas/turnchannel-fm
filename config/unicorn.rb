ba00bd1a0e2f8a53bb34793e084f01f65ccda330
shawnjanas <janasshawn@gmail.com>
2012-10-30 01:20:01 -0400

    ..

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,10 +7,23 @@ preload_app true
 @resque_web_pid = nil
 
 before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+
   @resque_pid ||= spawn("bundle exec rake " + \
   "resque:work QUEUES=*")
 
   @resque_schedule_pid ||= spawn("bundle exec rake " + \
   "resque:scheduler")
   @resque_web_pid ||= spawn("resque-web")
+end
+
+after_fork do |server, worker|
+  # Replace with MongoDB or whatever
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
 end
