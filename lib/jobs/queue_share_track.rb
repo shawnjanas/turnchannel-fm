28a2e4430ea246a36330b1bcfdbf78896bdf1883
shawnjanas <janasshawn@gmail.com>
2012-11-02 06:51:45 -0400

    Remove buffer app gem

--- lib/jobs/queue_share_track.rb
+++ lib/jobs/queue_share_track.rb
@@ -3,7 +3,7 @@ class QueueShareTrack
 
   def self.perform(track_id)
     if (track = Track.find_by_id(track_id))
-      message = "Checkout #{track.artist} new track \"#{track.title}\" featured on turnchannel"
+      message = "Checkout #{track.artist} new track \"#{track.title}\" featured on TurnChannel"
       HTTParty.post 'https://api.bufferapp.com/1/updates/create.json?access_token=1/6538074d7e3ff8fe02a8d458c3071390', :body => {
         :text => "#{message} http://turnchannel.com/tracks/#{track.permalink}",
         :profile_ids => ['506ba34c1b81f6393c00001a', '506ba3061b81f6513c00001e'],
