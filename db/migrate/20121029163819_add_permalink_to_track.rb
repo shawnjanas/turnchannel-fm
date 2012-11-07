b079274cfd66cc76932dc01dd02576227a16d2cb
shawnjanas <janasshawn@gmail.com>
2012-11-07 09:58:25 -0500

    Remove deleted files

--- db/migrate/20121029163819_add_permalink_to_track.rb
+++
@@ -1,9 +0,0 @@
-class AddPermalinkToTrack < ActiveRecord::Migration
-  def self.up
-    add_column :tracks, :permalink, :string
-    add_index :tracks, :permalink
-  end
-  def self.down
-    remove_column :tracks, :permalink
-  end
-end
