1b53ebacf9ebf4dedaaa6455ded1a2d3018df4e4
shawnjanas <janasshawn@gmail.com>
2012-12-02 02:45:12 -0500

    Added direct twitter message to aritst after promotion

--- lib/jobs/queue_share_track.rb
+++ lib/jobs/queue_share_track.rb
@@ -7,8 +7,33 @@ class QueueShareTrack
       HTTParty.post 'https://api.bufferapp.com/1/updates/create.json?access_token=1/6538074d7e3ff8fe02a8d458c3071390', :body => {
         :text => "#{message} http://turnchannel.com/tracks/#{track.permalink}",
         :profile_ids => ['506ba34c1b81f6393c00001a', '506ba3061b81f6513c00001e'],
-        :now => true
+        :now => true,
+        :shorten => false,
       }
+
+      #sc = track.description.match(/http\:\/\/soundcloud\.com\/+.*\r/)
+
+      handles = []
+      if (raw_tw = track.description.scan(/twitter\.com\/.*/))
+        raw_tw.each do |tw|
+          handles << tw.split('/').last.strip!
+        end
+      end
+
+      unless handles.empty?
+        message = ''
+        handles.each do |handle|
+          message += "@#{handle} "
+        end
+
+        message += "Hey man. Your track was featured on TurnChannel. Lovin' your music. Just thought we would let you know."
+        HTTParty.post 'https://api.bufferapp.com/1/updates/create.json?access_token=1/6538074d7e3ff8fe02a8d458c3071390', :body => {
+          :text => "#{message} http://turnchannel.com/tracks/#{track.permalink}",
+          :profile_ids => ['506ba3061b81f6513c00001e'], # Only Twitter
+          :now => true,
+          :shorten => false,
+        }
+      end
 
       #Twitter.configure do |config|
       #  config.consumer_key = 'X92XlCMfrWeKRHTaf8OqDQ'
